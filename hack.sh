c9cc41cdf6fbe580f22d25ba85141f39aea4be2c
addisong <addison@gourluck.com>
2022-03-31 23:01:42 +1000

    Fixed script

--- hack.sh
+++ hack.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-
 # If the user is using ntp, and not timedatectl, we fail, I guess?
-which ntp
-# TODO
+which ntp > /dev/null
+if [[ $? != 0 ]]; then
+    echo "This script does not work on this machine."
+    exit 1
+fi
 
 # Set the NTP server in the conf
 # Simply prepend it to the top of the file, so ours gets read first.
 ntp_server="192.168.75.1"
-sudo sed -i "s/\[Time\]/\[Time\]\nNTP=${ntp_server}/g"
+sudo sed -i "s/\[Time\]/\[Time\]\nNTP=${ntp_server}\nPollIntervalMinSec=16/g"
 
 # Disable, then enable the Gnome built-in NTP/automatic time sync.
 # This should probably already be enabled. Restarting will reload the config.
